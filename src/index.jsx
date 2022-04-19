55e7ef0dbeee8453dc8be6999b20f2c026a5bcd3
G51DHU <gs_sidhu@hotmail.co.uk>
2022-04-19 10:33:16 +0100

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import ReactDOM from "react-dom";
+
+function Index(){
+  return(
+    <>
+      <h1>Welcome to React Vite Micro App!</h1>
+      <p>Hard to get more minimal than this React app.</p>
+    </>
+  );
+}
+
+ReactDOM.render(<Index />, document.getElementById("root"));
+
