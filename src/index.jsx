e9a3e09270b5cd6f468c3e25710cec79a2cd99a2
G51DHU <gs_sidhu@hotmail.co.uk>
2022-04-21 22:28:12 +0100

    Removed "h1" and "p" tag.

--- src/index.jsx
+++ src/index.jsx
@@ -4,8 +4,6 @@ import AddSoftware from "./pages/add-software";
 function Index(){
   return(
     <>
-      <h1>Welcome to React Vite Micro App!</h1>
-      <p>Hard to get more minimal than this React app.</p>
       <AddSoftware />
     </>
   );
