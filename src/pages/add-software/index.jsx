3fc73c70376a7f4df90f720193d5501d8ed9bf55
G51DHU <gs_sidhu@hotmail.co.uk>
2022-04-21 22:29:05 +0100

    Fixed fetch request.

--- src/pages/add-software/index.jsx
+++ src/pages/add-software/index.jsx
@@ -1,14 +1,38 @@
+import { useEffect, useState } from "react"
+
 export default function AddSoftware(){
+    
+    const [listOfSoftware, SetListOfSoftware] = useState([])
 
-    const list_of_software = fetch("http://localhost:8000/list_of_software")
-        .then(response => {return response.json()})
-    console.log(list_of_software)
-
+    useEffect(()=>{
+        fetch("http://localhost:8000/list_of_software")
+            .then(response => response.json())
+            .then(data => SetListOfSoftware(data))
+    },[])
 
     return (
-        <div>
-            wow
-            {list_of_software.map((software)=>(<>{software}</>))}
+        <div className="add-software">
+            <h1>Add Software</h1>
+
+            <input type="search" placeholder="Search for software..." />
+
+            {
+            listOfSoftware.map((software, index)=>(
+                <div key={index} >
+                    <div>
+                        {software.name}
+                    </div>
+                    <div>
+                        {software.version}
+                    </div>
+                    <div>
+                        {software._id}
+                    </div>
+                </div>
+
+            ))
+            }
+
         </div>
     )
 }   
