a5b257ae6a957fa3dc179bcc07cf2199280df784
G51DHU <gs_sidhu@hotmail.co.uk>
2022-04-28 19:57:40 +0100

    :D

--- src/pages/software/existing-software/index.jsx
+++ src/pages/software/existing-software/index.jsx
@@ -2,8 +2,11 @@ import './style.css'
 import SearchIcon from '../../../assets/magnifying-glass.svg'
 import { useEffect, useState } from 'react'
 import Software from './software'
+
 export default function ViewSoftware () {
-  const [listOfSoftware, SetListOfSoftware] = useState([])
+  const [ListOfSoftware, SetListOfSoftware] = useState([])
+  const [SoftwareView, SetSoftwareView] = useState([])
+  const [SearchQuery, SetSearchQuery] = useState("")
 
   useEffect(() => {
     window.fetch('http://localhost:8000/list_of_software')
@@ -11,19 +14,33 @@ export default function ViewSoftware () {
       .then(data => SetListOfSoftware(data))
   }, [])
 
+  if (SearchQuery !== ""){
+    ListOfSoftware.forEach((software)=>{
+      if (software.name.includes(SearchQuery)){
+        SoftwareView.push(software)
+      }
+    })
+  }
+  else{
+    SoftwareView === ListOfSoftware
+  }
+
+
   return (
     <div className='existing-software'>
       <h2>Existing software</h2>
 
       <div className='software__search-bar'>
         <img className='software__magnifying-glass' src={SearchIcon} />
-        <input className='software__input' type='search' placeholder='Search for existing software...' />
+        <input className='software__input' type='search' placeholder='Search for existing software...' value={SearchQuery} onChange={e => SetSearchQuery(e.target.value)} />
       </div>
 
-      <div className='software__list'>
-        {
-          listOfSoftware.map((software, index) => <Software key={index} name={software.name} version={software.version} _id={software._id} />)
-        }
+      <div className="software__list-wrapper">
+        <div className='software__list'>
+          {
+            SoftwareView.map((software, index) => <Software key={index} name={software.name} version={software.version} _id={software._id} />)
+          }
+        </div>
       </div>
     </div>
   )
--- src/pages/software/index.jsx
+++ src/pages/software/index.jsx
@@ -1,14 +1,28 @@
 import './style.css'
 import ExistingSoftware from './existing-software'
 import AddSoftware from './add-software'
+import { useState } from 'react'
+import ViewSoftware from './existing-software'
+
 export default function Software () {
+  const STYLE = {
+    "button-active":{
+      "backgroundColor" : "grey"
+    }
+  }
+  
+  const [active, setActive] = useState("view")
+
   return (
     <div className='software'>
       <h1 className='software__title'>Software</h1>
-      <div className='software__view-and-add'>
-        <ExistingSoftware />
-        <AddSoftware />
+      <div className='software__view-and-add-switch'>
+        <button className='software__view-software-button' style={active === "view" ? STYLE["button-active"] : null} onClick={e => setActive("view")} > View Software </button>
+        <button className='software__add-software-button' style={active === "add" ? STYLE["button-active"] : null} onClick={e => setActive("add")} > Add software </button>
       </div>
+        {
+          active === "view" ? <ViewSoftware /> : <AddSoftware />
+        }
     </div>
   )
 }
