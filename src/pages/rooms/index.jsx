93916487e217b86d86cf8a360ef1a396214ab792
G51DHU <gs_sidhu@hotmail.co.uk>
2022-05-05 10:37:07 +0100

    .

--- src/pages/rooms/index.jsx
+++ src/pages/rooms/index.jsx
@@ -7,7 +7,7 @@ export default function Rooms() {
     const [ListOfSoftware, SetListOfSoftware] = useState([])
 
     function GetRooms () {
-        window.fetch('http://192.168.1.211:8000/rooms')
+        window.fetch('http://localhost:8000/rooms')
           .then(response => response.json())
           .then(data => SetListOfRooms(data))
     }
@@ -16,7 +16,7 @@ export default function Rooms() {
     },[])
 
     function GetSoftware () {
-        window.fetch('http://192.168.1.211:8000/software')
+        window.fetch('http://localhost:8000/software')
           .then(response => response.json())
           .then(data => SetListOfSoftware(data))
     }
@@ -28,6 +28,8 @@ export default function Rooms() {
                 <div className="rooms__row">
                     {ListOfRooms.map((room, index)=> <Room key={index} _id={room._id} name={room.name} software={room.software_id} />)}
                 </div>
+            </div>
+            <div className="rooms__add--deactivated">
             </div>
         </div>
     )
